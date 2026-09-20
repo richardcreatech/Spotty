fb593c5f43c2e8499f54299043fe8e69790b85d1
richardcreatech <richardcreatech@gmail.com>
2026-09-21 00:12:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [artist, setArtist] = useState(null);
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async () => {
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(
+      `http://localhost:5000/spotify/search?q=${encodeURIComponent(search)}`,
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    setResults(data.artists.items);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/spotify/artist/6fWVd57NKTalqvmjRd2t8Z")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setArtist(data);
+      });
+  }, []);
+
+  return (
+    <main className="spotify-page">
+      <div className="neon-glow glow-green"></div>
+      <div className="neon-glow glow-pink"></div>
+
+      <div className="floating-heart heart-one">♥</div>
+      <div className="floating-heart heart-two">♥</div>
+      <div className="floating-heart heart-three">♥</div>
+
+      <section className="search-container">
+        <p className="spotify-label">SPOTIFY DISCOVER</p>
+
+        <h1>
+          Find your
+          <span> sound.</span>
+        </h1>
+
+        <p className="subtitle">
+          Search for artists and discover what Spotify has to offer.
+        </p>
+
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search for an artist..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+
+          <button onClick={handleSearch}>🔍</button>
+        </div>
+
+        <div className="results">
+          {results.map((artist) => (
+            <div className="artist-result" key={artist.id}>
+              <img src={artist.images[0]?.url} alt={artist.name} />
+
+              <div>
+                <h2>{artist.name}</h2>
+                <p>Artist</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
